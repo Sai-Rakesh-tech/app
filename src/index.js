4d59a3a6f62b2fbabe49ee53c69dad84a96fe4c6
Sai-Rakesh-tech <gsairakesh0204@gmail.com>
2025-05-20 21:01:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('Love app is running!');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
