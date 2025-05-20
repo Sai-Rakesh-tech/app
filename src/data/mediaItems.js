e0ee728600c0f147651940098ee6becdc768225c
Sai-Rakesh-tech <gsairakesh0204@gmail.com>
2025-05-20 19:33:34 +0530

    Create mediaItems.js

---
+++ src/data/mediaItems.js
@@ -0,0 +1,10 @@
+const mediaItems = [
+  {
+    id: 1,
+    image: "/media/image1.jpg", // Replace with your image paths
+    caption: "You're my favorite weirdo ❤️"
+  },
+  // Add more media items here
+];
+
+export default mediaItems;
