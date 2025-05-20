e3d3eadc4900a88308bb0bfc8c3d6c5eebb0607d
Sai-Rakesh-tech <gsairakesh0204@gmail.com>
2025-05-20 20:59:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import GamePage from './components/GamePage';
+import PuzzlePage from './components/PuzzlePage';
+import UnlockPage from './components/UnlockPage';
 
-function App() {
-  return <div>Hello, World!</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/puzzle" element={<PuzzlePage />} />
+        <Route path="/unlock" element={<UnlockPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
-
-// import React from 'react';
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-// import LandingPage from './components/LandingPage';
-// import GamePage from './components/GamePage';
-// import PuzzlePage from './components/PuzzlePage';
-// import UnlockPage from './components/UnlockPage';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/game" element={<GamePage />} />
-//         <Route path="/puzzle" element={<PuzzlePage />} />
-//         <Route path="/unlock" element={<UnlockPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
